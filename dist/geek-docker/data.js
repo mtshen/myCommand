b74e9c63591bd30447662b623cc8c562fdded0bc
申孟涛 <mtshen1997@163.com>
2021-01-10 08:19:37 +0800

    delete dist 目录

--- dist/geek-docker/data.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_1 = require("../constants/DataStorage/index");
-exports["default"] = new index_1["default"]();
--- dist/option/axiosConf.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var axios_1 = require("axios");
-axios_1["default"].defaults.withCredentials = true;
